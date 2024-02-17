84e50a5031c9d2202435057cf14032a3ee6a8e2e
mangeshahirayy <97844580+xylus11@users.noreply.github.com>
2024-02-17 13:15:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,30 +6,16 @@ const App = () => {
 
   const onClick = () => {
     setCount(count + 1);
-    console.log(count); // blue
-    if (count >= 10) {
-      setCount(0);
     }
   };
 
-  const onClickDecrement = () => {
-    console.log(count); // blue
-    if (count > 0) {
-      setCount(count - 1);
-    }
-  };
 
-  const styling = {
-    // Set color based on count
-    display: count === 1 ? 'none' : 'inline',
-    width: 80 // Set background color based on count
-  };
 
   return (
     <div>
-      <button onClick={onClick}>Increment</button>
-      <button onClick={onClickDecrement}>Decrement</button>
-      <div style={styling}>{count}</div>
+
+  <p>Button clicked {count} times</p>
+    <button onClick={onClick}>Click me</button>
     </div>
   );
 };
