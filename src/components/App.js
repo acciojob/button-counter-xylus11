0d61a97b56756b5e12b023f32ba5d8b300874c9c
mangeshahirayy <97844580+xylus11@users.noreply.github.com>
2024-02-17 13:07:20 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  const onClick = () => {
+    setCount(count + 1);
+    console.log(count); // blue
+    if (count >= 10) {
+      setCount(0);
+    }
+  };
+
+  const onClickDecrement = () => {
+    console.log(count); // blue
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  const styling = {
+    // Set color based on count
+    display: count === 1 ? 'none' : 'inline',
+    width: 80 // Set background color based on count
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <button onClick={onClick}>Increment</button>
+      <button onClick={onClickDecrement}>Decrement</button>
+      <div style={styling}>{count}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
